b9100d96e77221efb51184f459f614ef5c234cfb
dzmitryvol4 <75065085+dzmitryvol4@users.noreply.github.com>
2021-04-21 18:02:15 +0300

    check values

---
+++ new/src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  public x: string = 'HEllo minchenok';
+  public user = {
+    name: 'John',
+    age: 23,
+  };
+  public myColor: string = 'red';
+  public myClass: string = 'red';
+  constructor() {
+    setTimeout(() => {
+      this.myClass = 'green';
+      setTimeout(() => {
+        this.myClass = 'blue';
+      }, 2000);
+    }, 2000);
+  }
+
+  random() {
+    return Math.random();
+  }
+
+  ngOnInit(): void {}
+}
